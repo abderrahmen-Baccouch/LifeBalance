52d74c273068064cfff0ac9f0ba3b51c486953b9
abderrahmen <abdoubaccouch42@gmail.com>
2022-04-12 18:40:53 +0100

    defit3 , create_repas ,item_repas

--- app/src/main/java/com/example/premierepage/FoodFragment.kt
+++ app/src/main/java/com/example/premierepage/FoodFragment.kt
@@ -1,19 +1,34 @@
 package com.example.premierepage
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class FoodFragment : Fragment() {
 
+    private lateinit var Btn: FloatingActionButton
+
+    @SuppressLint("UseRequireInsteadOfGet")
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        Btn = view!!.findViewById(R.id.fab)
+        Btn.setOnClickListener {
+            val intent = Intent(context,create_repas::class.java)
+            startActivity(intent)
+        }
+    }
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food, container, false)
+        super.onCreate(savedInstanceState)
+        return inflater.inflate(R.layout.fragment_food,container,false)
     }
 }
