589635e2f3eaa378b04f9b21bce5b39d31344cd3
abderrahmen <abdoubaccouch42@gmail.com>
2022-04-18 02:30:52 +0100

    les recettes

---
+++ app/src/main/java/com/example/premierepage/lunch9.kt
@@ -0,0 +1,11 @@
+package com.example.premierepage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class lunch9 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lunch9)
+    }
+}
