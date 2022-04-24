7bbd9b610429853edf990dfb91c099405537fc54
abderrahmen <abdoubaccouch42@gmail.com>
2022-04-24 20:39:29 +0100

    repas admin

---
+++ app/src/main/java/com/example/premierepage/GestionRepasAdmin.kt
@@ -0,0 +1,11 @@
+package com.example.premierepage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GestionRepasAdmin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gestion_repas_admin)
+    }
+}
